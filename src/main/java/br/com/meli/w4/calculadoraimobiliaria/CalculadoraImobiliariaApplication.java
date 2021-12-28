45ba69e1005f39baaafa3b58cb0526bdc8df4433
vfeitoza <vinicius.feitoza@mercadolivre.com>
2021-12-28 12:42:24 -0300

    Initial commit

---
+++ src/main/java/br/com/meli/w4/calculadoraimobiliaria/CalculadoraImobiliariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.w4.calculadoraimobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraImobiliariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculadoraImobiliariaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/meli/w4/calculadoraimobiliaria/CalculadoraImobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.w4.calculadoraimobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraImobiliariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
